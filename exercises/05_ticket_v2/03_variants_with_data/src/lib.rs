1876228490ae61da64c9e5fe92e2678180e5eb43
Dalelukdune <tornike.tsulukidze@gmail.com>
2026-09-09 17:04:42 +0400

    Make solution compiler-driven refactoring

--- exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
+++ exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
@@ -36,7 +36,9 @@ impl Ticket {
     pub fn assigned_to(&self) -> &str {
         match &self.status {
             Status::InProgress { assigned_to } => assigned_to,
-            _ => panic!("Only `In-Progress` tickets can be assigned to someone"),
+            Status::Done | Status::ToDo => {
+                panic!("Only `In-Progress` tickets can be assigned to someone")
+            },
         }
     }
 }
